33976012e3955d1c39efca68d10e5ddb04145ea8
Derek <kerafyrm415@outlook.com>
2025-04-08 18:25:46 -0700

    init commit

---
+++ src/main/java/com/example/practica6poo/Practica6PooApplication.java
@@ -0,0 +1,13 @@
+package com.example.practica6poo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Practica6PooApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Practica6PooApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/practica6poo/Practica6PooApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.practica6poo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Practica6PooApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
